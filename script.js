a8a88d8ae1f1a0cda01177352955c7288fe6d930
VARUN KHARKWAL <varunkh12345@gmail.com>
2023-09-07 17:49:39 +0530

    slideshow scripted

---
+++ script.js
@@ -0,0 +1,26 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+}
+
+// Automatically advance to the next slide every 3 seconds
+setInterval(nextSlide, 3000);
+
+// Initial display
+showSlide(currentSlide);
