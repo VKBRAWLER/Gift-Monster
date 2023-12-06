5d30c8e932d1e313f6383fecc8c2a505927c2ace
Varun Kharkwal <varunkh12345@gmail.com>
2023-12-06 11:06:02 +0530

    removed slideshow from homepage

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// SlideShow Animation for index.html
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        if (i === index) { slide.style.display = 'block'; } 
-        else { slide.style.display = 'none'; }
-    });}
-function nextSlide() {
-    currentSlide++;
-    if (currentSlide >= slides.length) { currentSlide = 0; }
-    showSlide(currentSlide); }
-// Automatically advance to the next slide every 3 seconds
-setInterval(nextSlide, 3000);
-// Initial display
-showSlide(currentSlide);
-
-
 //Button to change image in item.html
 const productImages = document.querySelectorAll('.imgBox .item-slide');
 const nextButton = document.querySelector('.nextBtn');
